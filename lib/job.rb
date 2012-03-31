0c1c93dea23426d0b348193102ebc325a9db454c
hideack <hideaki@feeddown.com>
2012-03-31 23:54:20 +0900

    Added key parameter for job object

--- lib/job.rb
+++ lib/job.rb
@@ -6,12 +6,14 @@ module Givalia
         attr_accessor :response
         attr_accessor :module
         attr_accessor :params
+        attr_accessor :key
 
         def initialize
             @id = nil
             @response = false
             @module = ""
             @params = nil
+            @key = nil
         end
     end
 end
