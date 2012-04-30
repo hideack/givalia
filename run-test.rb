936332603b2d6193e1d6d3afbfb9959869b9274c
hideack <hideaki@feeddown.com>
2012-04-30 15:39:32 +0900

    Added comments.

--- run-test.rb
+++ run-test.rb
@@ -1,12 +1,19 @@
 #!/usr/bin/ruby
+# givalia test cases
+#
+# $./run-test.rb
+# $./run-test.rb -n test_send_enq_command
+# $./run-test.rb -t TestClientFunction
+#
 
 require 'test/unit'
-
-test_file = "test/test_*.rb"
 
 $:.unshift(File.join(File.expand_path("."), "lib"))
 $:.unshift(File.join(File.expand_path("."), "test"))
 
+test_file = "test/test_*.rb"
+
+# Test start
 Dir.glob(test_file) do |file|
     require './' + file.sub(/\.rb$/, '')
 end
